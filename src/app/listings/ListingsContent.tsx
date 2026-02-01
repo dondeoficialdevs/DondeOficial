0985ce4c3f7a0d725524b0bb4b19ece230f599e9
edissonpinza98 <edizon963@gmail.com>
2026-02-01 13:03:55 -0500

    Corecion de despliegue por error fix use client

--- src/app/listings/ListingsContent.tsx
+++ src/app/listings/ListingsContent.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useState, useEffect, useMemo } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
 import { businessApi, categoryApi } from '../../lib/api';
